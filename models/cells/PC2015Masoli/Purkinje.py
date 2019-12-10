335c7244a85486caae37e30934caa8dfaae3c4ec
appukuttan-shailesh <appukuttan.shailesh@gmail.com>
2019-12-10 12:52:43 +0100

    bytes to string

--- models/cells/PC2015Masoli/Purkinje.py
+++ models/cells/PC2015Masoli/Purkinje.py
@@ -206,7 +206,7 @@ class Purkinje:
 		for para in self.subsets_paraextra:
 			for d in self.ModelViewParmSubset[int(para[0])]:
 				d.insert(para[1].decode('UTF-8'))
-				exec('d.gmax_'+para[2]+' = '+str(para[3]))
+				exec('d.gmax_'+para[2].decode('UTF-8')+' = '+para[3].decode('UTF-8'))
 
 		listgmax = []
 		for d in self.ModelViewParmSubset[2]:
